c867913bda220416db0ffb9b014d6300e4ab6083
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2023-07-28 15:55:03 -0300

    28.07

---
+++ excJava/1074.java
@@ -0,0 +1,12 @@
+import java.IOException;
+import java.util.Scanner;
+
+public class 1074{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        for(int i=1; i <=N; i++){
+         if(i % == 0 || i > 0) System.out.println("EVEN POSITIVE")
+        }
+    }
+}
