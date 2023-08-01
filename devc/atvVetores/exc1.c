911929cc4cda9fa9ee14aa4a7e037e7391572170
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2023-08-01 15:09:50 -0300

    atvRevisao

---
+++ devc/atvVetores/exc1.c
@@ -0,0 +1,21 @@
+//ARRUMAR
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h> 
+int main(){
+
+    int vetor[9], i;
+
+    for (i=0; i<9; i++){
+        printf("\n vetor[%d]: ",i);
+        scanf("%d",&vetor[i]);
+        scanf("%d", &vetor);
+        
+        if(i % 2 == 0){
+         printf("%d", i);
+        }
+    } 
+    
+        
+    
+}
---
+++ devc/atvVetores/exc2.c
@@ -0,0 +1,102 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h> 
+int main(){
+    int i, indiceMaior, indiceMenor; 
+    float menor = 99, maior = 0, temp[12];
+    
+     for(i = 0; i < 12; i++) { 
+        printf("Digite a temperatura do mes %d:\n ", i + 1);
+         scanf("%f", &temp[i]); 
+     } 
+     
+     for(i = 0; i < 12; i++) { 
+        if(menor > temp[i]) { 
+            menor = temp[i]; indiceMenor = i;
+        }
+        if(maior < temp[i]) {
+            maior = temp[i]; indiceMaior = i; 
+        } 
+     }
+        printf("A menor temperatura foi %.0fºC e ocorreu no mes ", menor); 
+      
+    switch(indiceMenor) { 
+        case 0: 
+            printf("Janeiro\n");
+            break;
+        case 1: 
+            printf("Fevereiro\n"); 
+            break; 
+        case 2: 
+            printf("Marco\n"); 
+            break; 
+        case 3: 
+            printf("Abril\n"); 
+            break; 
+        case 4: 
+            printf("Maio\n"); 
+            break; 
+        case 5: 
+            printf("Junho\n"); 
+            break; 
+        case 6: 
+            printf("Julho\n"); 
+            break; 
+        case 7: 
+            printf("Agosto\n"); 
+            break; 
+        case 8: 
+            printf("Setembro\n"); 
+            break; 
+        case 9: 
+            printf("Outubro\n"); 
+            break; 
+        case 10: 
+            printf("Novembro\n"); 
+            break; 
+        case 11: 
+            printf("Dezembro\n");
+            break; 
+         }
+    
+     printf("A maior temperatura foi %.0fºC e ocorreu no mes ", maior); 
+     
+     switch(indiceMaior) { 
+         case 0: 
+            printf("Janeiro\n");
+            break;
+        case 1: 
+            printf("Fevereiro\n"); 
+            break; 
+        case 2: 
+            printf("Marco\n"); 
+            break; 
+        case 3: 
+            printf("Abril\n"); 
+            break; 
+        case 4: 
+            printf("Maio\n"); 
+            break; 
+        case 5: 
+            printf("Junho\n"); 
+            break; 
+        case 6: 
+            printf("Julho\n"); 
+            break; 
+        case 7: 
+            printf("Agosto\n"); 
+            break; 
+        case 8: 
+            printf("Setembro\n"); 
+            break; 
+        case 9: 
+            printf("Outubro\n"); 
+            break; 
+        case 10: 
+            printf("Novembro\n"); 
+            break; 
+        case 11: 
+            printf("Dezembro\n");
+            break; 
+     }  
+}
