7ea18b31f4ee96767fcd741301c03ac8480c6204
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2023-08-06 18:52:28 -0300

    .

--- excJava/1038.java
+++ excJava/1038.java
@@ -1,27 +1,41 @@
-import java.io.IOException;
 import java.util.Scanner;
+import java.io.IOException;
 
 public class 1038 {
-		public static void main(String[] args) throws IOException {
-        Scanner s = new Scanner(System.in);
-        int qnt = s.nextInt();
-        int cod = s.nextInt();
-        double total = 0.0;
-
-    if (cod == 1) {
-		total = qnt * 4.0;
-	} else if (cod == 2) {
-		total = qnt * 4.50;
-	} else if (cod == 3) {
-		total = qnt * 5.0;
-	} else if (cod == 4) {
-		total = qnt *2.0;
-	} else if (cod == 5) {
-		total = qnt * 1.50;
-	} else {
-		System.out.println("Codigo nao encontrado.");
-	}
-		System.out.println(String.format("Total: R$ %.2f", total));
-    }
- }
-
+    public static void main(String[] args)throws IOException {
+		Scanner s = new Scanner(System.in);
+		
+		String linha = s.nextLine();
+		String[] valores = linha.split("\\s");
+		
+		int codigo = Integer.parseInt(valores[0]);
+		int quantidade = Integer.parseInt(valores[1]);
+		double total = 0;
+		
+		if(codigo == 1) {
+			total = quantidade * 4.0;
+			System.out.printf("Total: R$ %.2f\n", total);
+		}
+		
+		else if(codigo == 2) {
+			total = quantidade * 4.5;
+			System.out.printf("Total: R$ %.2f\n", total);
+		}
+		
+		else if(codigo == 3) {
+			total = quantidade * 5.0;
+			System.out.printf("Total: R$ %.2f\n", total);
+		}
+		
+		else if(codigo == 4) {
+			total = quantidade * 2.0;
+			System.out.printf("Total: R$ %.2f\n", total);
+		}
+		
+		else if(codigo == 5) {
+			total = quantidade * 1.5;
+			System.out.printf("Total: R$ %.2f\n", total);
+		}
+			
+	}//static void
+}
