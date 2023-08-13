c0e8cc50aab2de7deb3179b8fa8d02d19662f645
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2023-08-13 12:27:18 -0300

    shine

---
+++ devc/atvString/desafio.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <locale.h>
+int Main(){
+    setlocale(LC_ALL,"Portuguese");
+    char nome[100];
+    int id_espacos[10];
+    int qtd_espacos = 0, ultimo_espaco = 0, num = 0;
+    printf("Digite seu nome completo: ");
+    gets(nome);
+
+    for(int i = 0; i < strlen(nome); i++){
+        if(nome[i] == ' '){
+            qtd_espacos++;
+            id_espacos[num] = i;
+            ultimo_espaco = i;
+            num++;
+        }
+    }
+
+    for(int i = 0; i < strlen(nome); i++){
+        if(i == 0){
+            printf("%c", toupper(nome[i]));
+        }
+        
+        else if(nome[i] == ' '){
+            break;
+        }
+
+        else{
+            printf("%c", nome[i]);
+        }
+    }
+
+    printf(" ");
+
+    for(int i = 0; i < qtd_espacos-1; i++){
+        printf("%c. ", toupper(nome[id_espacos[i]+1]));
+    }
+
+    for(int i = ultimo_espaco+1; i < strlen(nome); i++){
+        if(i == ultimo_espaco+1){
+            printf("%c", toupper(nome[i]));
+        }else{
+            printf("%c", nome[i]);
+        }
+
+        if(i == strlen(nome)-1) printf("\n");
+    }
+
+}//main
