eb3b553202475b846f3d1da5fc9235abd893e439
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2023-08-20 11:31:43 -0300

    2

--- devc/atvMatriz/exc2.c
+++ devc/atvMatriz/exc2.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 #include<stdio.h>
-int main() {
-    int i,j; 
-    float A[2][2], maior;
-     int B[0][0];
+int main(){
+    int A[2][2];
+    int maior=0;
+
+    for (int i=0;i<2;i++){ 
+        for (int j=0;j<2;j++){
+            scanf("%d",&A[i][j]);
+            if (A[i][j] > maior) maior = A[i][j]; 
+        }      
+    }
+    for (int i=0;i<2;i++){ 
+        for (int j=0;j<2;j++){
+            A[i][j] *= maior;
+        }    
+    }
     
-     printf("Digite uma matriz 2x2:\n"); 
-     for (i=0; i<2; i++){ 	
-         for (j=0; j<2; j++) 	 
-         scanf("%f", &A[i][j]); 	 
-     }		 
-     
-     maior=A[0][0]; 	 
-     for(i=0;i<=2;i++){ 	 
-         for(j=0;j<=2;j++){ 	 
-             if(maior<A[i][j]){ 	 
-                 maior=A[i][j]; 
-                 
-             } 
-         } 
-     }
-     printf("\nO maior elemento da matriz eh: %.0f\n", maior);	
-     
-     
-    for (i = 0; i < A[i][j]; i++) {
-        for (j = 0; j < A[i][j]; j++) {
-            B[i][j] = A[i][j] * maior;
-            printf("O valor da matriz resultante é: %.0f\n ", B[i][j]);
-        }
+    printf("Maior número da matriz: %d\n",maior);
+    printf("Nova matriz B:\n");
+
+    for (int i=0;i<2;i++){ 
+        for (int j=0;j<2;j++){
+            printf("%d ",A[i][j]);
+        }    
+        printf("\n");  
     }
 }
