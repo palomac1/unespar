360f49d5238b04ce4f7f220a3d0f2d5a0aec5d36
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2023-09-15 14:14:36 -0300

    java

--- devc/atvStruct/exc2.c
+++ devc/atvStruct/exc2.c
@@ -1,54 +1,58 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-	struct habitantes{
-		float salario;
-		int idade;
-		char sexo;
-		int filhos;
-		
-	};
-		
-	typedef struct habitantes Habitantes;
+    typedef struct habitantes{
+        double salario;
+        int idade;
+        char sexo;
+        int num_filhos;
+    }Habitantes;
 
 int main(){
-    Habitantes h[0];
-    int i = 0, contf;
-    float X;
-        
-	while(i != 1000){
-		printf("Digite o salario da pessoa: \n");
-		scanf("%f", &h[i].salario);
 
-		printf("Digite a idade da pessoa: \n");
-		scanf("%d", &h[i].idade);
+    int qtd, i=0;
+    double cont_sal_inferior=0.0;
+    double menor_idade = 0.0,total_sal = 0.0,total_filhos=0.0, maior_sal=0.0;
 
-        if(h[i].idade == 0) break;
+    printf("Quantos usuários deseja cadastrar? ");
+    scanf("%d",&qtd);
+    Habitantes h[qtd];
 
-		printf("Digite o sexo da pessoa: \n");
-		scanf("%s", &h[i].sexo);
-
-        printf("Digite a quantidade de filhos da pessoa: \n");
-		scanf("%d", &h[i].filhos);
-
-        printf("\n");
-        i++;
-
-        if(sexo == 'F' && salario < 300){
-            X+= h[i].idade;
-            contf++;
+    while (i != qtd){
+        printf("\nDigite o salário: ");
+        scanf("%lf",&h[i].salario);
+        total_sal += h[i].salario;
+        maior_sal = h[i].salario;
+        if (maior_sal> h[i].salario){
+            maior_sal= h[i].salario;
         }
 
-        printf("Media da idade das mulheres %f", X/contf);
-	}
+        printf("\nDigite a idade: ");
+        scanf("%d",&h[i].idade);
+        menor_idade = h[i].idade;
 
-   
+        if (h[i].idade < menor_idade){
+            menor_idade = h[i].idade; 
+        }
 
+        if (h[i].idade == 0) break;
 
+        printf("\nDigite o sexo: ");
+        scanf("%s",&h[i].sexo);
 
+        printf("\nDigite o numero de filhos: ");
+        scanf("%d",&h[i].num_filhos);
+        total_filhos += h[i].num_filhos;
 
+        if (h[i].salario < 300 && h[i].sexo == 'f') cont_sal_inferior++;
+        i++;
+    }
 
-	
+    printf("Média de mulheres com salário menor de 300 reais: %lf \n",cont_sal_inferior/i);
+    printf("Média de salário da população: %.2lf\n",total_sal/i);
+    printf("Média de número de filhos: %.2lf\n",total_filhos/i);
+    printf("Maior salário: %.2lf\n",maior_sal);
+    printf("Menor idade: %.2lf\n",menor_idade);
 
-   
 }
+
