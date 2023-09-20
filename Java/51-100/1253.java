fa69989ef7e9ebbeda28ffdd4810f2dfcc989c5d
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2023-09-20 17:26:33 -0300

    1253

--- Java/51-100/1253.java
+++ Java/51-100/1253.java
@@ -1,34 +1,24 @@
-import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
- 
-        Scanner obj=new Scanner(System.in);
-        int testcase=obj.nextInt();
-        String s=obj.nextLine();
-        int v=0;
+public class 1253 {
 
-        for (int o=0;o<testcase;o++){
-            s=obj.nextLine();
-            if (v!=0)
-            s=obj.nextLine();
-            int n=obj.nextInt();
-            char[] a=new char[s.length()];
-            
-            for (int i=0;i<s.length();i++){
-                int z=((int)s.charAt(i)-n);
-                if(z<65)
-                {
-                    z=z+26    ;
-                }
-                a[i]=(char)z;
-                System.out.print(a[i]);
-            }
-            
-            System.out.print("\n");
-            v++;
-        }
-    }
-  }
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		for (int i=0 ; i<N ; i++){
+			sc.nextLine();
+			String str = sc.nextLine();
+			int troca=sc.nextInt();
+			char[] s = str.toCharArray();
+			for (int x=0 ; x<s.length ; x++){
+				if((s[x]-troca) < 65)
+					s[x]+=(26-troca);
+				else
+					s[x]-=troca;
+			}
+			System.out.println(s);
+		}
+		sc.close();
+	}
+}
