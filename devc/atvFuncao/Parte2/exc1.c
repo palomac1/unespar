3849cd9282e642f06bbe7815880d942027fd3b2b
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2023-09-26 11:08:46 -0300

    exc funcao

--- devc/atvFuncao/Parte2/exc1.c
+++ devc/atvFuncao/Parte2/exc1.c
@@ -1,3 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+
+	void mediat(float nota1, float nota2,float nota3, char letra, float *media){	
+	if(letra == 'A'){
+		*media = (nota1 + nota2 + nota3)/3; 
+	} else { 
+		*media = ((nota1 * 5) + (nota2 * 3) + (nota3 * 2)) / 10;
+	}
+} 
+	int main(){
+		float nota1, nota2, nota3, media;
+		char letra;
+		printf("Digite a nota 1: \n");
+		scanf("%f", &nota1);
+		printf("Digite a nota 2: \n");
+		scanf("%f", &nota2);
+		printf("Digite a nota 3: \n");
+		scanf("%f", &nota3);
+		fflush(stdin);
+		printf("Digite a letra: \n");
+		scanf("%c", &letra);
+		mediat(nota1, nota2, nota3, letra, &media);
+	
+		printf("A media do aluno e: %.2f \n", media);
+		
+		return 0;
+	}
---
+++ devc/atvFuncao/Parte2/exc2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+void produto(float valorAtual, valorAntigo, float *percentual){	
+    *percentual = (valorAtual - valorAntigo) * 100/valorAntigo;
+} 
+
+int main(){
+		float valorAntigo, valorAtual, percentual;
+
+        printf("Digite o valor atual desse produto: \n");
+        scanf("%f", &valorAtual);
+        printf("Digite o valor antigo desse produto: \n");
+        scanf("%f", &valorAntigo);
+        
+        produto(valorAtual, valorAntigo, &percentual);
+
+        printf("Valor do acrescimo: %.3f%% \n", percentual);
+
+        return 0;
+}
