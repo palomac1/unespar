36771bbb378dcc0577bc44782c897181ad2168ef
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2023-09-26 21:43:03 -0300

    4

--- devc/atvFuncao/Parte2/exc4.c
+++ devc/atvFuncao/Parte2/exc4.c
@@ -1,20 +1,21 @@
 #include<stdlib.h>
 
-	void vetor(int vet[25]){
-			for(int i = 0; i < 25; i++){
-				if(vet[i] < 0){
-					vet[i] = 0;
-				}			
-			}
-        }
-	int main(){
-		int vet[25],valor;
+void vetor(int vet[25]){
+	for(int i = 0; i < 25; i++){
+		if(vet[i] < 0){
+			vet[i] = 0;
+		}			
+	}
+}
+
+int main(){
+	int vet[25],valor;
         for(int i = 0; i < 25; i++){
-		printf("Digite os valores: \n");
-		scanf("%d", &vet[i]);   
+		 printf("Digite os valores: \n");
+		 scanf("%d", &vet[i]);   
 		}	
-        vetor(vet);
-        for(int i = 0; i < 25; i++){
-        printf("Valores: %d \n", vet[i]);
+         vetor(vet);
+         for(int i = 0; i < 25; i++){
+         printf("Valores: %d \n", vet[i]);
 	}
 }
