0c0cd6b58d8a33c87a5a7d193ec60657362c6695
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2023-11-14 14:40:58 -0300

    ponteiros

--- devc/atvPonteiros/exc4.c
+++ devc/atvPonteiros/exc4.c
@@ -1,25 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-void troca(int *x, int *y, int z){
-     z = *x;
-     *x = *y;
-     *y = z;
+void troca(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 int main(){
-    int x, y;
-
-    printf("Digite um valor para X: ");
-    scanf("%d", &x);
-    printf("Digite um valor para Y: ");
-    scanf("%d", &y);
-
-    troca(&x, &y);
-
-    printf("Valores depois da troca: \n");
-    printf("Novo valor de X: %d", x).
-    printf("Novo valor de Y: %d", y);
-
-    return 0;
+	int a, b;
+	
+	printf("Digite um valor para a: ");
+	scanf("%d", &a);
+	printf("Digite um valor para b: ");
+	scanf("%d", &b);
+	
+	troca(&a, &b);
+	
+	printf("O valor de a: %d\n", a);
+	printf("O valor de b: %d", b);
+	
+	return 0;
 }
