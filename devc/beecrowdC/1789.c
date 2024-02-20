d17732f25805469e8bdc810305572e9cbb2928d5
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2024-02-20 07:47:07 -0300

    ,

--- devc/beecrowdC/1789.c
+++ devc/beecrowdC/1789.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 
-int main (){
+int main() {
+    int n, maior, velocidade;
 
-  unsigned short int n, maior, velocidade;
+    while (scanf("%d", &n) != EOF) {
+        int vetor[n];
+        for (int i = 0; i < n; i++)
+            scanf("%d", &vetor[i]);
 
-  while (scanf("%hd", &n) != EOF){
-    int vetor[n];
-    for (unsigned i = 0; i < n; i++)
-      scanf("%d", &vetor[i]);
+        maior = vetor[0];
+        for (int i = 1; i < n; i++) {
+            if (vetor[i] > maior)
+                maior = vetor[i];
+        }
 
-    maior = vetor[0];
-    for (unsigned i = 1; i < n; i++)
-    {
-
-      if (vetor[i] > maior)
-        maior = vetor[i];
-
+        if (maior < 10)
+            printf("1\n");
+        else if (maior >= 10 && maior < 20)
+            printf("2\n");
+        else if (maior >= 20)
+            printf("3\n");
     }
 
-    if (maior < 10)
-      printf("1\n");
-    else if ( maior >= 10 && maior < 20)
-      printf("2\n");
-    else if (maior >= 20)
-      printf("3\n");
-
-  }
+    return 0;
 }
